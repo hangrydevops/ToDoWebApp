2e5123f7c74807de2a7518b7fc2e25ad7783a5f1
Devin <67713580+hangrydevops@users.noreply.github.com>
2020-07-10 12:54:46 -0500

    Add files via upload

--- create.php
+++ create.php
@@ -10,7 +10,7 @@
  <p>Todo description: </p>
  <input name=”todoDescription” type=”text”>
  <br>
- <input type=”submit” name=”submit” value=”submit”>
+ <input type=”submit” name=submit value=”submit”>
 </form>
 </body>
 </html>
@@ -18,6 +18,25 @@
  if(isset($_POST[‘submit’])) {
     $title = $_POST[‘todoTitle’];
     $description = $_POST[‘todoDescription’];
-echo “you filled title “ . $title . “<br> and description “ . $description;
+echo 'you filled title ' . $title . '<br> and description ' . $description;
 }
+?>
+ <?php
+ require_once(“db_connect.php”);
+ if(isset($_POST[‘submit’])) {
+     $title = $_POST[‘todoTitle’];
+     $description = $_POST[‘todoDescription’];
+     //connect to database
+    db();
+     global $link;
+     $query = 'INSERT INTO todo(todoTitle, todoDescription, date)     VALUES (‘$title’, ‘$description’, now() )';
+    $insertTodo = mysqli_query($link, $query);
+    if($insertTodo){
+       echo “successfully”;
+    }else{
+        echo mysqli_error($link);
+    }
+    mysqli_close($link);
+
+ }
 ?>
--- db_connect.php
+++ db_connect.php
@@ -3,7 +3,7 @@
 <?php
 function db(){
     global $link;
-    $link = mysqli_connect(“localhost”, “root”, “”, “tododb_270122”) or     die(“couldn’t connect to database”);
+    $link = mysqli_connect('localhost', 'root', '', 'tododb_270122') or die('couldn’t connect to database');
     return $link;
 }
 ?>
